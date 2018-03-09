ac675885930c69727530877a596f9d617f5b8edb
Miguel Aybar <maaybars@gmail.com>
2018-03-10 00:41:36 +0100

    Corrige reseteo al arranque.
    Cambio de pin de activacion de relé. El pin P3 tiene el diodo del USB que al arrancar pone 2.3 voltios.
    Esto ocasiona que el relé se active durante el arranque producioendo un bloqueo del micro.

--- CalderaCtrl.ino
+++ CalderaCtrl.ino
@@ -17,16 +17,19 @@
 #include <avr/wdt.h>
  
 #define LEN_BUFFER_RX  8
-#define PIN_TX    0
+#define PIN_RELE  0
 #define PIN_LED   1
 #define PIN_RX    2
-#define PIN_RELE  3
+#define PIN_TX    3
+
+#define SAFE_TIMEOUT 330 //segundos
+#define HIGH_SPEED  24028
+#define LOW_SPEED  50000
 
 uint8_t radio_msg[LEN_BUFFER_RX];  //Data transmission buffer
 uint8_t buflen = sizeof(radio_msg);
 double count=0;
-double count_max=0;
-#define SAFE_TIMEOUT 180 //segundos
+double count_max=LOW_SPEED;
 double safe_counter=0;
 bool in =false;
   
@@ -35,22 +38,20 @@ bool in =false;
 */
 void setup()
 {
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_RELE, OUTPUT);
+  digitalWrite(PIN_RELE, false);
+  digitalWrite(PIN_LED, false);
+  
   wdt_disable();    // No te olvides
-  delay(1000);
-  //SerialUSB.begin(); 
+ 
   vw_set_tx_pin(PIN_TX);
   vw_set_rx_pin(PIN_RX);
 
   vw_setup(2000);   // Bits per sec
   vw_rx_start();       // Start the receiver PLL running 
 
-  pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_RELE, OUTPUT);
-  digitalWrite(PIN_RELE, false);
-  digitalWrite(PIN_LED, false);
-
   wdt_enable(WDTO_1S);
-
 }
 
 void loop() 
@@ -63,7 +64,7 @@ void loop()
     if(comparar("0001-ON_", 8) == 0){
       digitalWrite(PIN_RELE, true);
       send("0100-ON-ACK");
-      count_max = 25000;
+      count_max = HIGH_SPEED;
       safe_counter = 0;
     }
     else if(comparar("0001-OFF", 8) == 0)
@@ -99,7 +100,7 @@ void stop_heater()
 {
   digitalWrite(PIN_RELE, false);
   send("0100-OFF-ACK");
-  count_max = 50000; 
+  count_max = LOW_SPEED; 
 }
 //Funcion para enviar el mensaje
 void send (char *message)
